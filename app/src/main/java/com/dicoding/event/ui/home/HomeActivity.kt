0b6487f4cef0af4beca453624c8353020594baa9
dimassaputra <dimas.maulanads@gmail.com>
2019-12-20 08:09:04 +0700

    DrawerLayout for main page

--- app/src/main/java/com/dicoding/event/ui/home/HomeActivity.kt
+++ app/src/main/java/com/dicoding/event/ui/home/HomeActivity.kt
@@ -3,9 +3,12 @@ package com.dicoding.event.ui.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.dicoding.event.R
 import com.dicoding.event.utils.UserPreference
 import com.dicoding.event.utils.getUserPreference
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
@@ -18,5 +21,18 @@ class HomeActivity : AppCompatActivity() {
         userPreference = getUserPreference()
 
         Toast.makeText(this, userPreference.toString(), Toast.LENGTH_LONG).show()
+
+        setSupportActionBar(activityHomeToolbar)
+
+        val drawerToggleButton = ActionBarDrawerToggle(this, activityHomeDrawerLayout, null, R.string.open, R.string.close)
+
+        activityHomeDrawerLayout.addDrawerListener(drawerToggleButton)
+        drawerToggleButton.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.icon_home)
+        supportActionBar?.title = ""
+        supportActionBar?.elevation = 0f
+
     }
 }
