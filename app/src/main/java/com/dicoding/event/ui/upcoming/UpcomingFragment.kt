7fba99e123f995292d43488604972d7d2c1fa13b
Dimas <dimas.maulanads@gmail.com>
2019-12-21 14:08:44 +0700

    Improve some code in UpcomingFragment

--- app/src/main/java/com/dicoding/event/ui/upcoming/UpcomingFragment.kt
+++ app/src/main/java/com/dicoding/event/ui/upcoming/UpcomingFragment.kt
@@ -8,17 +8,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
 import com.dicoding.event.R
 import com.dicoding.event.data.remote.model.EventsResponse
 import com.dicoding.event.ext.invisible
 import com.dicoding.event.ext.visible
+import com.dicoding.event.ui.home.HomeEventRecyclerAdapter
 import com.dicoding.event.utils.Error
 import com.dicoding.event.utils.Loading
 import com.dicoding.event.utils.Success
-import kotlinx.android.synthetic.main.fragment_upcoming.*
+import com.dicoding.event.utils.ViewModelState
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
@@ -36,48 +37,47 @@ class UpcomingFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var progressBar: ProgressBar
 
-    private val adapter = UpcomingRecyclerAdapter()
+    private val adapter = HomeEventRecyclerAdapter().apply {
+        events = this@UpcomingFragment.events
+    }
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_upcoming, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-
-        recyclerView = view.findViewById(R.id.fragmentUpcomingRecyclerView)
         progressBar = view.findViewById(R.id.fragmentUpcomingProgressBar)
-
-        adapter.events = events
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView = view.findViewById<RecyclerView>(R.id.fragmentUpcomingRecyclerView).apply {
+            adapter = this@UpcomingFragment.adapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
 
         upcomingViewModel.getUpcomingEvent()
-        upcomingViewModel.observerState.observeForever {
+        upcomingViewModel.observerState.observe(this, observableState)
+    }
 
-            when (it) {
-                is Loading -> {
-                    progressBar.visible()
-                    recyclerView.invisible()
-                }
 
-                is Success<*> -> {
-                    events = it.data as List<EventsResponse.Event>
-                    adapter.events = events
-                    recyclerView.visible()
-                    progressBar.invisible()
-                }
+    private val observableState = Observer<ViewModelState> {
+        when(it) {
+            is Loading -> {
+                progressBar.visible()
+                recyclerView.invisible()
+            }
 
-                is Error -> {
-                    Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
-                    progressBar.invisible()
-                }
+            is Success<*> -> {
+                events = it.data as List<EventsResponse.Event>
+                adapter.events = events
+
+                progressBar.invisible()
+                recyclerView.visible()
+            }
+
+            is Error -> {
+                Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                progressBar.invisible()
             }
         }
     }
